e8e49f1bb1e9e7876c397cf668ee57d73bbea3f8
fanismichalakis <45755277+fanismichalakis@users.noreply.github.com>
2020-12-12 19:05:26 +0100

    fetch and display real articles (still ugly)

--- app/src/main/java/fr/fanismichalakis/monactu/MainActivity.kt
+++ app/src/main/java/fr/fanismichalakis/monactu/MainActivity.kt
@@ -1,25 +1,24 @@
 package fr.fanismichalakis.monactu
 
-import android.content.Context
-import android.net.ConnectivityManager
-import android.net.NetworkCapabilities
 import android.os.Build
 import android.os.Bundle
-import android.service.autofill.Dataset
 import android.util.Log
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import android.widget.TextView
-import android.widget.Toast
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.android.volley.Request
+import com.android.volley.Response
 import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
+import org.json.JSONArray
+import org.json.JSONObject
+import java.util.*
+import kotlin.collections.ArrayList
+import kotlin.collections.HashMap
 
 
 class MainActivity : AppCompatActivity() {
@@ -38,16 +37,18 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        initDataset()
+        //initDataset()
+        getArticles()
+        //initMockDataset()
 
-        viewManager = LinearLayoutManager(this)
+        /*viewManager = LinearLayoutManager(this)
         viewAdapter = CustomAdapter(dataset)
 
         recyclerView = findViewById<RecyclerView>(R.id.recyclerViewArticles).apply {
             setHasFixedSize(true)
             layoutManager = viewManager
             adapter = viewAdapter
-        }
+        }*/
 
 
         val builder: AlertDialog.Builder? = MainActivity@ this?.let {
@@ -96,25 +97,56 @@ class MainActivity : AppCompatActivity() {
         queue.add(stringRequest)
     }
 
-    private fun initDataset() {
+    private fun initMockDataset() {
         dataset = ArrayList<ArticlesObject>()
 
         var article = ArticlesObject("A la Une", "Etienne Klein")
         dataset.add(article)
+        article = ArticlesObject("A la deux", "Max Weber")
+        dataset.add(article)
     }
 
-    private fun getArticles(articleSource: String, apiKey: String) {
-        val url = "https://newsapi.org/v2/everything?apiKey=$apiKey&language=fr&sources=$articleSource"
+    /*private fun initDataset() {
+        dataset = ArrayList<ArticlesObject>()
+
+        // add articles to dataset
+        dataset = getArticles(dataset)
+        Log.d("initDataset", "dataset = ${dataset.toString()}")
+    }*/
+
+    private fun getArticles() {
+        dataset = ArrayList<ArticlesObject>()
+        // Instantiate the RequestQueue.
         val queue = Volley.newRequestQueue(this)
-        val stringRequest = object: JsonObjectRequest(
-                Request.Method.GET, url, null,
-                { response ->
-                    Log.d("TAG", response.toString())
-                },
-                { error ->
-                    Log.d("TAG", "Something went wrong: $error")
-                    //TODO("need to make show dialog a function outside of onCreate")
-                })
+        val url: String = "https://newsapi.org/v2/everything?apiKey=d59958a4990048c896539cb17af6a6b7&language=fr&sources=google-news-fr"
+
+        val req = object : JsonObjectRequest(url, null,
+        Response.Listener{ response ->
+            val strResp = response.toString()
+            val jsonObj: JSONObject = JSONObject(strResp)
+            Log.d("getArticles", "JSON of articles : $jsonObj")
+            Log.d("getArticles", "JSON.toString() of articles : ${jsonObj.toString()}")
+            val jsonArray: JSONArray = jsonObj.getJSONArray("articles")
+            var strArticleTitle: String = ""
+            var strArticleAuthor: String = ""
+            var article = ArticlesObject(strArticleTitle,strArticleTitle)
+            for (i in 0 until jsonArray.length()) {
+                val jsonInner: JSONObject = jsonArray.getJSONObject(i)
+                strArticleTitle = jsonInner.get("title").toString()
+                Log.d("getArticles", "title: $strArticleTitle")
+                strArticleAuthor = jsonInner.get("author").toString()
+                Log.d("getArticles", "author; $strArticleAuthor")
+                article = ArticlesObject(strArticleTitle, strArticleAuthor)
+                Log.d("article", "next article: ${article.toString()}")
+                dataset.add(article)
+                Log.d("dataset", "updated dataset: $dataset")
+            }
+            Log.d("dataset", "dataset après boucle for: $dataset")
+            triggerRecyclerView()
+        },
+        Response.ErrorListener { error ->
+            Log.e("getArticles", "Something went wrong $error")
+        })
         {
             override fun getHeaders(): MutableMap<String, String> {
                 val headers = HashMap<String, String>()
@@ -122,8 +154,19 @@ class MainActivity : AppCompatActivity() {
                 return headers
             }
         }
+        Log.d("dataset", "dataset in getArticles : $dataset")
+        queue.add(req)
 
-        queue.add(stringRequest)
+    }
+
+    private fun triggerRecyclerView() {
+        viewManager = LinearLayoutManager(this)
+        viewAdapter = CustomAdapter(dataset)
+
+        recyclerView = findViewById<RecyclerView>(R.id.recyclerViewArticles).apply {
+            setHasFixedSize(true)
+            layoutManager = viewManager
+            adapter = viewAdapter
+        }
     }
 }
-
