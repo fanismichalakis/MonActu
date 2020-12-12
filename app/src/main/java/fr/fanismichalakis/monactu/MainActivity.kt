bc78d4131d666f78e7d7171e1683797d1f3be3cd
fanismichalakis <45755277+fanismichalakis@users.noreply.github.com>
2020-12-12 10:27:10 +0100

    add no connection alert dialog

--- app/src/main/java/fr/fanismichalakis/monactu/MainActivity.kt
+++ app/src/main/java/fr/fanismichalakis/monactu/MainActivity.kt
@@ -1,20 +1,47 @@
 package fr.fanismichalakis.monactu
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
-import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
+import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
 import com.android.volley.Request
-import com.android.volley.Response
 import com.android.volley.toolbox.JsonObjectRequest
-import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
 
+
 class MainActivity : AppCompatActivity() {
+
+    @RequiresApi(Build.VERSION_CODES.M)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val context: Context = applicationContext
+
+
+        val builder: AlertDialog.Builder? = MainActivity@ this?.let {
+            AlertDialog.Builder(it)
+        }
+        builder?.setMessage(R.string.no_connection_message)
+                ?.setTitle(R.string.no_connection_dialog_title)
+                ?.apply {
+                    setPositiveButton(R.string.ok_button
+                    ) { dialog, id ->
+                    //User clicked OK button
+                    }
+                    setNegativeButton(R.string.cancel_button
+                    ) { dialog, id ->
+                    //User cancelled the dialog
+                    }
+                }
+        val dialog: AlertDialog? = builder?.create()
+
 
         val apiKey = "d59958a4990048c896539cb17af6a6b7"
         val textViewSources = findViewById<TextView>(R.id.textViewSources)
@@ -24,14 +51,15 @@ class MainActivity : AppCompatActivity() {
 
         // Request a string response from the provided URL.
         val stringRequest = object: JsonObjectRequest(
-            Request.Method.GET, url, null,
-            { response ->
-                Log.d("TAG", response.toString())
-                textViewSources.text = response.toString()
-            },
-            { error ->
-                Log.d("TAG", "Something went wrong: $error")
-                textViewSources.text = "Something went wrong"})
+                Request.Method.GET, url, null,
+                { response ->
+                    Log.d("TAG", response.toString())
+                    textViewSources.text = response.toString()
+                },
+                { error ->
+                    Log.d("TAG", "Something went wrong: $error")
+                    dialog?.show()
+                })
         {
             override fun getHeaders(): MutableMap<String, String> {
                 val headers = HashMap<String, String>()
@@ -42,5 +70,4 @@ class MainActivity : AppCompatActivity() {
 
         queue.add(stringRequest)
     }
-
 }
